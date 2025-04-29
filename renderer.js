0fc655b36df968c25fc5d66e51dbe8ade29f79ae
Héloïs <127110080+hlelo101@users.noreply.github.com>
2025-04-29 20:18:44 +0200

    Delete everything

--- renderer.js
+++
@@ -1,9 +0,0 @@
-const time = document.getElementById('currentTime');
-
-function updateTime() {
-    const currentDate = new Date();
-    time.innerHTML = currentDate.getHours() + ':' + currentDate.getMinutes();
-}
-
-updateTime();
-setTimeout(updateTime, 30000);
